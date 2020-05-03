e6ec624b9e4dc1e6b9130fa5577e48f028f0e1b7
Guillermo Rauch <rauchg@gmail.com>
2020-05-03 15:31:43 -0700

    new

---
+++ footer.jsx
@@ -0,0 +1,7 @@
+
+{/*<Footer>
+  <div style={{ padding: '10px', fontSize: '12px', textAlign: 'right' }}>
+    Use ← / → keys or swipe to navigate. Built by <a href="https://twitter.com/rauchg" target="_blank">@rauchg</a> – <a href="https://github.com/rauchg/berkshide-deck-demo">Source code</a>
+  </div>
+</Footer>*/}
+
---
+++ provider.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { useDeck } from "mdx-deck";
+
+export default (props) => {
+  const state = useDeck();
+
+  return (
+    <div>
+      {props.children}
+
+      <div
+        style={{
+          position: "absolute",
+          left: 0,
+          right: 0,
+          fontSize: 14,
+          padding: "15px 30px",
+          bottom: 0,
+        }}
+      >
+        Use ← → or swipe to navigate. By{" "}
+        <a href="https://twitter.com/rauchg" target="_blank">
+          @rauchg
+        </a>{" "}
+        <a href="https://github.com/rauchg/berkshire-dekc-demo" target="_blank">
+          (source)
+        </a>
+      </div>
+    </div>
+  );
+};
